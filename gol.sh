e09953f6e59cf0fbfe89ac974d13663d048fc8e8
Jonas Stare <yownas@gmail.com>
2021-10-17 20:34:57 +0200

    666 bytes \o/

--- gol.sh
+++ gol.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-P='^[^O]*(O[^O]*){';z='s/O/[OS]/g;s/ /[ B]/g;y'
+P='^ *(O *){';z='s/O/[OS]/g;s/ /[ B]/g;y/o_/O /'
 o=$(cat);E=$(printf "%$(wc -L<<<$o|sed 's/ .*//')s");e=$(tr \  .<<<$E)
 Q='s/\(...\)\(.\)\(.\)\(...\)/s%\\(\1e\2\\)T\\(\3e\4\\)%\\1f\\2%g;ts;/g;'
-n=$((echo 2o;seq -sp 255)|dc|sed 's/^/0000000/;s/^.*\(........\)$/\1/;y/01/ O/')
-m=$(echo ":s;";egrep "$P"'2,3}$'<<<$n|sed "$(sed 's/e/'$e'/g;y/Tf/oS/'<<<$Q)"|sed "$z/o/O/";\
-egrep "$P"'3}$'<<<$n|sed "$(sed 's/e/'$e'/g;y/Tf/_B/'<<<$Q)"|sed "$z/_/ /")
-while [ "$o" != "$l" ];do 
-  l="$o";o=$((echo " $E ";sed 's/^/ /;s/$/ /'<<<$l;echo " $E ")|tr '#_\012' O\ -|\
-  sed "$m"|tr OSB- ' OO\012'|sed 's/^.//;s/.$//'|tail -n+2|head -n-1)
-  clear;echo "$o";echo "Generation: $(( ++i ))";sleep 1
+n=$((echo 2o;seq -sp 255)|dc|sed -E 's/^/0000000/;s/^.*(.{8})$/\1/;y/01/ O/')
+m=$(echo ":s;";egrep "$P"'2,3}$'<<<$n|sed "$(sed 's/e/'$e'/g;y/Tf/oS/'<<<$Q)"|sed "$z";\
+egrep "$P"'3}$'<<<$n|sed "$(sed 's/e/'$e'/g;y/Tf/_B/'<<<$Q)"|sed "$z")
+while [ "$o" != "$l" ];do l="$o";o=$((echo " $E ";sed 's/^/ /;s/$/ /'<<<$l;echo " $E ")|\
+tr '\012' -|sed "$m"|tr OSB- ' OO\012'|sed 's/^.//;s/.$//'|tail -n+2|head -n-1)
+clear;echo "$o";echo "Gen: $(( ++i ))";sleep 1
 done
