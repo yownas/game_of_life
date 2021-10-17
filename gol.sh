b0a68d7e0cfda4f1fa4cb8a8952227f8693ef2f5
Jonas Stare <yownas@gmail.com>
2021-10-17 20:08:33 +0200

    Shave off 99 bytes

--- gol.sh
+++ gol.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cat > /tmp/gol;l=i=0;E=$(printf "%$(wc -L /tmp/gol|sed 's/ .*//')s");e=$(echo -n "$E"|sed 'y/ /./')
-P='^[^O]*(O[^O]*){';x='s/\(...\)\(.\)\(.\)\(...\)/s%\\(\1';y='\\2%g;ts;/g;';z='s/O/[OS]/g;s/ /[ B]/g;y'
-n=$( (echo 2o;seq -s p 0 256)|dc|sed 's/^/0000000/;s/^.*\(........\)$/\1/;y/01/ O/');m=$(echo ":s;";
-echo "$n" | egrep "$P"'2,3}$' | sed $x$e'\2\\)o\\(\3'$e'\4\\)%\\1S'$y | sed "$z/o/O/";\
-echo "$n" | egrep "$P"'3}$' | sed $x$e'\2\\)_\\(\3'$e'\4\\)%\\1B'$y | sed "$z/_/ /")
-while [ "$o" != "$l" ]; do 
-  l=$o
-  o=$( (echo " $E ";sed 's/^/ /;s/$/ /' < /tmp/gol; echo " $E ") | tr '#_\012' 'O -' | sed "$m" |\
-  tr 'OSB-' ' OO\012' | sed 's/^.//;s/.$//' | tail -n+2 | head -n-1 | tee /tmp/gol.)
-  clear; echo "$o"; echo "Generation: $(( ++i ))"; mv /tmp/gol. /tmp/gol; sleep 1
+P='^[^O]*(O[^O]*){';z='s/O/[OS]/g;s/ /[ B]/g;y'
+o=$(cat);E=$(printf "%$(wc -L<<<$o|sed 's/ .*//')s");e=$(tr \  .<<<$E)
+Q='s/\(...\)\(.\)\(.\)\(...\)/s%\\(\1e\2\\)T\\(\3e\4\\)%\\1f\\2%g;ts;/g;'
+n=$((echo 2o;seq -sp 255)|dc|sed 's/^/0000000/;s/^.*\(........\)$/\1/;y/01/ O/')
+m=$(echo ":s;";egrep "$P"'2,3}$'<<<$n|sed "$(sed 's/e/'$e'/g;y/Tf/oS/'<<<$Q)"|sed "$z/o/O/";\
+egrep "$P"'3}$'<<<$n|sed "$(sed 's/e/'$e'/g;y/Tf/_B/'<<<$Q)"|sed "$z/_/ /")
+while [ "$o" != "$l" ];do 
+  l="$o";o=$((echo " $E ";sed 's/^/ /;s/$/ /'<<<$l;echo " $E ")|tr '#_\012' O\ -|\
+  sed "$m"|tr OSB- ' OO\012'|sed 's/^.//;s/.$//'|tail -n+2|head -n-1)
+  clear;echo "$o";echo "Generation: $(( ++i ))";sleep 1
 done
