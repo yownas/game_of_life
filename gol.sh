557a5832b26491c6b8555a1951037077484a1c52
Jonas Stare <yownas@gmail.com>
2021-10-21 19:59:23 +0200

    ...removed a line

--- gol.sh
+++ gol.sh
@@ -4,4 +4,3 @@ n=$((echo 2o;seq -sp 225)|dc|xargs printf "%.8i\n"|tr 01 \ O);z='soOo[OS]og;si i
 Q='BEGIN{FS=""}/^ *(O *){3}$/{print ":L"++i";sw("$1$2$3"'$e'"$4")o("$5"'$e'"$6$7$8")w\\1z\\2w;tL"i";"}'
 m=$(gawk "$(sed s1312,31<<<$Q)"<<<$n|sed "$z;yxozxOSx";gawk "$Q"<<<$n|sed "$z;yeLozel Be")";sy.-.y\nyg;ysOSBs OOs"
 while o=$(sed -Ez "ys\ns-s;sy-y - yg;$m"<<<"$E+-$o-$E+"|grep -v +);do clear;echo -e "$o\nGen: $(( ++i ))";done
-
