a31000bc6b911d74f9d0c819efedd44ea9547625
Jonas Stare <yownas@gmail.com>
2021-10-21 23:08:37 +0200

    Add space to make the code nice and clean

--- gol.sh
+++ gol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 o=$(cat);E=$(printf %$(wc -L<<<$o|sed sz' .*'zz)s);e=$(tr \  .<<<$E) #  Silly implementation of Game of Life
-n=$((echo 2o;seq -sp 225)|dc|xargs printf "%.8i\n"|tr 01 \ O);z='soOo[OS]og;si i[ +B]ig;ysoz' #    by Yownas
+n=$( (echo 2o;seq -sp 225)|dc|xargs printf "%.8i\n"|tr 01 \ O);z='soOo[OS]og;si i[ +B]ig;ysoz' #   by Yownas
 Q='BEGIN{FS=""}/^ *(O *){3}$/{print ";:L"++i";sw("$1$2$3"'$e'"$4")o("$5"'$e'"$6$7$8")w\\1z\\2w;tL"i}' #  :)
 m=$(gawk "$(sed s1312,31<<<$Q)"<<<$n|sed "${z}sOSs";gawk "$Q"<<<$n|sed "${z}Ls Bls")";sy.-.y\nyg;ysOSBs OOs"
 while o=$(sed -Ez "ys\ns-s;sy-y - yg$m"<<<"$E+-$o-$E+"|grep -v +);do clear;printf "$o\nGen: $(( ++i ))";done
